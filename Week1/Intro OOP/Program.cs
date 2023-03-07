8b836462c9cbaf529dfac343349911ef31a34976
andreaguli14 <andrea.gulisano1@live.it>
2023-03-07 14:24:14 +0100

    giorno 1 e 2

---
+++ Week1/Intro OOP/Program.cs
@@ -0,0 +1,70 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Atleta atleta = new Atleta("andrea","gulisano","basket");
+        Dipendente dipendente = new Dipendente("Kris","Camilleri","IT Manager");
+        Animale animale = new Animale("Gatto", 3);
+        Veicolo veicolo = new Veicolo("Nissan", "Juke", "AA000AA");
+
+        Console.WriteLine($"L'atleta si chiama {atleta.nome} {atleta.cognome} e fa {atleta.sport}");
+        Console.WriteLine($"Il dipendente si chiama {dipendente.nome} {dipendente.cognome} e il suo lavoro e` {dipendente.lavoro}");
+        Console.WriteLine($"L'animale e` un {animale.specie} e ha {animale.eta} anni");
+        Console.WriteLine($"Il veicolo e`una {veicolo.marca} il modello e`{veicolo.modello} ed e`targata {veicolo.targa}");
+    }
+
+}
+
+class Atleta
+{
+   public string nome { get; }
+   public string cognome { get; }
+   public string sport { get; }
+
+    public Atleta(string nome, string cognome, string sport)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.sport = sport;
+    }
+}
+
+class Dipendente
+{
+    public string nome { get; }
+    public string cognome { get; }
+    public string lavoro { get; }
+
+    public Dipendente(string nome, string cognome, string lavoro)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.lavoro = lavoro;
+    }
+}
+
+class Animale
+{
+    public string specie { get; }
+    public int eta { get; }
+
+    public Animale(string specie, int eta)
+    {
+        this.specie = specie;
+        this.eta = eta;
+    }
+}
+
+class Veicolo
+{
+    public string marca { get; }
+    public string modello { get; }
+    public string targa { get; }
+
+    public Veicolo(string marca, string modello, string targa)
+    {
+        this.marca = marca;
+        this.modello = modello;
+        this.targa = targa;
+    }
+}
---
+++ Week1/OOP 1/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+        List<Persona> persone = new List<Persona>();
+        persone.Add(new Persona("Mario","Rossi",45));
+        persone.Add(new Persona("Andrea", "Gulisano", 18));
+        persone.Add(new Persona("Rosario", "Bianchi", 32));
+
+
+        Console.WriteLine("Persone:\n------------");
+        foreach (Persona Persona in persone)
+        {
+            Console.WriteLine(Persona.GetNome());
+            Console.WriteLine(Persona.GetCognome());
+            Console.WriteLine(Persona.GetEta());
+            Console.WriteLine(Persona.GetDettagli());
+            Console.WriteLine("------------");
+
+        }
+    }
+}
+
+class Persona
+{
+    string nome { get; }
+    string cognome { get; }
+    int eta { get; }
+
+    public Persona(string nome, string cognome, int eta)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+    public string GetNome()
+    {
+        return this.nome;
+    }
+
+    public string GetCognome()
+    {
+        return this.cognome;
+    }
+    public int GetEta()
+    {
+        return this.eta;
+    }
+
+    public string GetDettagli()
+    {
+        return $"Dettagli completi:\nNome:{this.nome}\nCognome:{this.cognome}\nEta`:{this.eta}";
+    }
+
+}
