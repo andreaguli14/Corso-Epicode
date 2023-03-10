36c6a8642baf8132c95d30db58a2cfec71e4b61f
andreaguli14 <andrea.gulisano1@live.it>
2023-03-10 17:33:05 +0100

    randomizzatore nomi

---
+++ Week1/Random/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+        List<string> gruppo = new List<string>();
+        gruppo.Add("franco");
+        gruppo.Add("armando");
+        gruppo.Add("saro");
+        gruppo.Add("andrea");
+
+        var test = new Test<string>();
+
+        gruppo = test.Shuffle(gruppo);
+        
+        foreach (var persona in gruppo) {
+
+            Console.WriteLine(persona);
+        }
+    }
+}
+
+public class Test<T>
+{
+    private Random random;
+
+    public Test()
+    {
+        random = new Random();
+    }
+
+    public List<T> Shuffle(List<T> list)
+    {
+      return list.OrderBy(x => random.Next()).ToList();
+    }
+}
